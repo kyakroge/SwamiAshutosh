66b95f656d3f0b783654a5b0e05d028df8e178a7
kyakroge <murlidharpathakstunter@gmail.com>
2023-06-06 21:34:00 +0530

    Updated header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,25 @@ import { faCoffee, faMusic } from '@fortawesome/free-solid-svg-icons';
 export class AppComponent {
   title = 'Swami Ashutoshanand';
 currentYear: any = 2023;
-faMusic = faMusic
+faMusic = faMusic;
+
+isBhakti: boolean = true;
+isBhakt:string='';
+updateBhakt() {
+  if (this.isBhakti==true) {
+    this.isBhakti=false;
+    this.isBhakt='Not a bhakt'
+  } else {
+    this.isBhakti=true;
+    this.isBhakt='Bhakt'
+  }
+}
+isDisabled: boolean = true;
+getTrue() {
+  this.isBhakti = true;
+}
+getFalse() {
+  this.isBhakti = false;
+}
+isSubscribedToEmailsMessage: string = "true";
 }
--- src/app/brandbar/brandbar.component.ts
+++ src/app/brandbar/brandbar.component.ts
@@ -7,27 +7,27 @@ import { NgModel } from "@angular/forms";
   styleUrls: ['./brandbar.component.scss']
 })
 export class BrandbarComponent {
-  isBhakti: boolean = true;
-  isBhakt:string=''
-  updateBhakt() {
-    if (this.isBhakti==true) {
-      this.isBhakti=false;
-      this.isBhakt='Not a bhakt'
-    } else {
-      this.isBhakti=true;
-      this.isBhakt='Bhakt'
-    }
-  }
-  isDisabled: boolean = true;
-  getTrue() {
-    this.isBhakti = true;
-  }
-  getFalse() {
-    this.isBhakti = false;
-  }
-  warn() {
-    // alert("hhh");
-  }
-  isSubscribedToEmailsMessage: string = "true";
+  // isBhakti: boolean = true;
+  // isBhakt:string=''
+  // updateBhakt() {
+  //   if (this.isBhakti==true) {
+  //     this.isBhakti=false;
+  //     this.isBhakt='Not a bhakt'
+  //   } else {
+  //     this.isBhakti=true;
+  //     this.isBhakt='Bhakt'
+  //   }
+  // }
+  // isDisabled: boolean = true;
+  // getTrue() {
+  //   this.isBhakti = true;
+  // }
+  // getFalse() {
+  //   this.isBhakti = false;
+  // }
+  // warn() {
+  //   // alert("hhh");
+  // }
+  // isSubscribedToEmailsMessage: string = "true";
 
 }

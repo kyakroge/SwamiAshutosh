3ff1756822e360eb6c91ba1a86cc5740fea44a9f
kyakroge <murlidharpathakstunter@gmail.com>
2023-06-15 03:13:57 +0530

    update UI

--- src/app/kailash-math/kailash-math.component.ts
+++ src/app/kailash-math/kailash-math.component.ts
@@ -28,14 +28,15 @@ export class KailashMathComponent {
 
     
     mahaMandaleswars: MahaMandaleswars[]=[
-      {name:'MM1', dob:'87', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM2', dob:'77', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM3', dob:'84', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM4', dob:'83', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM5', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM6', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM7', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
-      {name:'MM8', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
+      {name:'महामंडलेश्वर स्वामी कैलाश पर्वत जी महाराज', dob:'87', imgUrl: 'https://ik.imagekit.io/kailash/maha-mandaleswars/kailash-parvat?updatedAt=1686760095607'},
+      {name:'महामंडलेश्वर स्वामी सत्यानंद गिरि जी महाराज', dob:'77', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
+      {name:'महामंडलेश्वर स्वामी पूर्णानन्द गिरि जी महाराज', dob:'84', imgUrl: 'https://ik.imagekit.io/kailash/maha-mandaleswars/poornanand?updatedAt=1686760159961'},
+      {name:'महामंडलेश्वर स्वामी कृपानन्द जी महाराज', dob:'83', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
+      {name:'महामंडलेश्वर स्वामी प्रेमपुरी जी महाराज', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/maha-mandaleswars/prempuri?updatedAt=1686760227621'},
+      {name:'महामंडलेश्वर स्वामी नारायण गिरि जी महाराज', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/maha-mandaleswars/narayan?updatedAt=1686759710287'},
+      {name:'महामंडलेश्वर स्वामी विष्णुदेवानंद गिरि जी महाराज', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/maha-mandaleswars/vishnudevanand'},
+      {name:'महामंडलेश्वर स्वामी प्रकाशानंद गिरि जी महाराज', dob:'97', imgUrl:'https://ik.imagekit.io/kailash/maha-mandaleswars/prakashanand?updatedAt=1686759994707'},
+      {name:'महामंडलेश्वर स्वामी रामचन्द्र गिरि जी महाराज', dob:'97', imgUrl: 'https://ik.imagekit.io/kailash/telegram/tr:w-200,f-jpg,pr-true/IMG-20230517-WA0008.jpg'},
       ];
   
 }
